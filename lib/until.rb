be54b5bdcd7aa359ac9494522c0cb582559f968d
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-04 00:22:41 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,10 @@
 def using_until
-	     levitation_force = 6
-	     until levitation_force == 10
-	        puts "Wingardium Leviosa"
-	        levitation_force += 1
-	    end
+  levitation_force = 6
+  
+  while levitation_force == 10
+  puts "Wingardium Leviosa"
+  levitation_force += 1
 end
+    
+end
+
